bdf76c63f784cdd4e3df67e937496bfab39b33f8
Gribek <piotrmaslewski@gmail.com>
2021-05-06 11:55:41 +0200

    Register runapp in the project

--- RunSchedule/settings.py
+++ RunSchedule/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'runapp.apps.RunappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
