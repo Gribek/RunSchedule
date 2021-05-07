fd09ca83e34f9a7e04ccf11fc4c865981ccb37ed
Gribek <piotrmaslewski@gmail.com>
2021-05-07 10:58:45 +0200

    Create ModelAdmin for custom user model

--- runapp/admin.py
+++ runapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """ModelAdmin for custom user model."""
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'is_admin')
+    list_filter = ('is_admin',)
+    search_fields = ('email',)
+    ordering = ('email',)
