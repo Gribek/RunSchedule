f2a8e6cb4fa1666e27c0d714236f54a3f04355b7
Gribek <piotrmaslewski@gmail.com>
2021-05-12 09:25:10 +0200

    Add docstrings to LandingPage and HomepageView class views

--- runapp/views.py
+++ runapp/views.py
@@ -4,11 +4,19 @@ from django.views.generic import TemplateView
 
 
 class LandingPage(View):
+    """Display application landing page."""
+
     def get(self, request):
+        """Display landing page or redirect to homepage.
+
+        Display landing page for non-authenticated users only,
+        redirect logged in users to the application homepage.
+        """
         if request.user.is_authenticated:
             return redirect('runapp:homepage')
         return render(request, 'runapp/landing_page.html')
 
 
 class HomepageView(TemplateView):
+    """Display application homepage."""
     template_name = 'runapp/homepage.html'
