4f3d85af8b6d8bfaf5ff92bb881ea099d3b70cb0
Gribek <piotrmaslewski@gmail.com>
2021-05-12 15:24:56 +0200

    Add url paths for login and logout
    Use django authentication views - LoginView and LogoutView.

--- runapp/urls.py
+++ runapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
 
 from runapp.views import LandingPageView, HomepageView
 
@@ -6,4 +7,7 @@ app_name = 'runapp'
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing_page'),
     path('home', HomepageView.as_view(), name='homepage'),
+    path('login', LoginView.as_view(template_name='runapp/login.html'),
+         name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
