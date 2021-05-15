aeb3e50259e5d984a81be392831a0c49ba74c49a
Gribek <piotrmaslewski@gmail.com>
2021-05-15 13:49:55 +0200

    Add model form for TrainingPlan model

--- runapp/forms.py
+++ runapp/forms.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 
-from runapp.models import User
+from runapp.models import User, TrainingPlan
+from runapp.widget import DatePicker
 
 
 class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email']
+
+
+class TrainingPlanForm(ModelForm):
+    class Meta:
+        model = TrainingPlan
+        exclude = ['owner']
+        widgets = {
+            'start_date': DatePicker(),
+            'end_date': DatePicker(),
+        }
+        labels = {
+            'current_plan': 'Set as current plan'
+        }
