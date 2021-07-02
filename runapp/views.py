4933938695ffa918a407cee49dde81e276a7e1de
Gribek <piotrmaslewski@gmail.com>
2021-07-02 15:51:33 +0200

    Change the order of import statements

--- runapp/views.py
+++ runapp/views.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from runapp.calendar import TrainingCalendar, get_date_today, str_to_datetime
 from runapp.forms import (UserForm, TrainingPlanForm, SelectCurrentPlanForm,
